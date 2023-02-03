7826c3a7c28a0124672b1afdcb34aafaad0f3086
fulgurance <zohran.londais@gmail.com>
2023-02-03 21:05:34 +0000

    Finished Pcre installer

--- Pcre/8.45/8.45.cr
+++ Pcre/8.45/8.45.cr
@@ -2,8 +2,14 @@ class Target < ISM::Software
     
     def configure
         super
-        runScript("autogen.sh",[""],buildDirectoryPath)
         configureSource([   "--prefix=/usr",
+                            "--docdir=/usr/share/doc/pcre-8.45",
+                            "--enable-unicode-properties",
+                            "--enable-pcre16",
+                            "--enable-pcre32",
+                            "--enable-pcregrep-libz",
+                            "--enable-pcregrep-libbz2",
+                            "--enable-pcretest-libreadline",
                             "--disable-static"],
                             buildDirectoryPath)
     end
