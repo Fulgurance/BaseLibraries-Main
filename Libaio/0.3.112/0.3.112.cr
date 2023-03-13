031e9c5857a4f9473c3af5c6bff2637ae3eedc25
fulgurance <zohran.londais@gmail.com>
2023-03-13 16:23:03 +0000

    Patched missing argument for path at prepare phase

--- Libaio/0.3.112/0.3.112.cr
+++ Libaio/0.3.112/0.3.112.cr
@@ -2,17 +2,20 @@ class Target < ISM::Software
 
     def prepare
         super
-        fileDeleteLine("#{buildDirectoryPath}src/Makefile",62)
+
+        fileDeleteLine("#{buildDirectoryPath(false)}src/Makefile",62)
     end
 
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
 
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
     end
 
 end
