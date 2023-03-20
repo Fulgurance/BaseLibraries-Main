06823e3774e03408dac7c8c1e23fce1a7a280d87
fulgurance <zohran.londais@gmail.com>
2023-03-20 10:52:57 +0000

    Added Glib installer

---
+++ Glib/2.68.4/2.68.4.cr
@@ -0,0 +1,37 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            "--buildtype=release",
+                            "-Dman=true",
+                            ".."],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand([] of String,buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4")
+
+        copyFile("#{buildDirectoryPath}docs/reference/NEWS","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/NEWS")
+        copyDirectory("#{buildDirectoryPath}docs/reference/gio","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/gio")
+        copyDirectory("#{buildDirectoryPath}docs/reference/glib","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/glib")
+        copyDirectory("#{buildDirectoryPath}docs/reference/gobject","#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/gobject")
+    end
+
+end
