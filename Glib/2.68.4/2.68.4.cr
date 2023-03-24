ae2c6172ee278fe81365129a9df6f9f58b16aa52
fulgurance <zohran.londais@gmail.com>
2023-03-24 22:23:26 +0000

    Removed file it doesn't exist

--- Glib/2.68.4/2.68.4.cr
+++ Glib/2.68.4/2.68.4.cr
@@ -28,7 +28,6 @@ class Target < ISM::Software
 
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4")
 
-        copyFile("#{buildDirectoryPath(false)}/docs/reference/NEWS","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/NEWS")
         copyDirectory("#{buildDirectoryPath(false)}/docs/reference/gio","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/gio")
         copyDirectory("#{buildDirectoryPath(false)}/docs/reference/glib","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/glib")
         copyDirectory("#{buildDirectoryPath(false)}/docs/reference/gobject","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/glib-2.68.4/gobject")
