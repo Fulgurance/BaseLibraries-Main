0f9e6c67af168d90233d4b17e5d1ebca191df1d6
fulgurance <zohran.londais@gmail.com>
2023-03-27 15:08:24 +0100

    Patched configure error

--- Ncurses/6.2.0/6.2.0.cr
+++ Ncurses/6.2.0/6.2.0.cr
@@ -19,7 +19,7 @@ class Target < ISM::Software
         if option("Pass1")
             configureSource([   "--prefix=/usr",
                                 "--host=#{Ism.settings.target}",
-                                "--build=$(./config.guess)",
+                                "--build=#{Ism.settings.target}",
                                 "--mandir=/usr/share/man",
                                 "--with-manpage-format=normal",
                                 "--with-shared",
