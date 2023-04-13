40aaec09d4712e10b95765e336140f0d0824bc4e
Fulgurance <zohran.londais@gmail.com>
2023-04-13 12:44:32 +0100

    Added boost library

---
+++ Boost/1.77.0/1.77.0.cr
@@ -0,0 +1,33 @@
+class Target < ISM::Software
+
+    def configure
+        super
+
+        runScript(  "bootstrap.sh",
+                    [   "--prefix=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr",
+                        "--with-python=python3"],
+                            buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        runScript(  "b2",
+                    [   "stage",
+                        "-j#{Ism.settings.makeOptions}",
+                        "threading=multi",
+                        "link=shared"],
+                            buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runScript(  "b2",
+                    [   "install",
+                        "threading=multi",
+                        "link=shared"],
+                            buildDirectoryPath)
+    end
+
+end
