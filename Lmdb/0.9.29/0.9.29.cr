45dc26ef1290d041c48fcbe801cbe07267ac471b
Fulgurance <zohran.londais@gmail.com>
2023-04-19 15:04:07 +0100

    PAtched extract phase

--- Lmdb/0.9.29/0.9.29.cr
+++ Lmdb/0.9.29/0.9.29.cr
@@ -1,5 +1,11 @@
 class Target < ISM::Software
 
+    def extract
+        super
+
+        moveFile("#{workDirectoryPath(false)}/lmdb-LMDB_0.9.29","#{workDirectoryPath(false)}/LMDB_0.9.29")
+    end
+
     def prepare
         @buildDirectory = true
         @buildDirectoryName = "libraries/liblmdb"
