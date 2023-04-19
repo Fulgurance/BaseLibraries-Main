ff615f700211fa0e56dc054180908d23e66a7f8c
Fulgurance <zohran.londais@gmail.com>
2023-04-19 15:06:56 +0100

    Patched missing / character

--- Lmdb/0.9.29/0.9.29.cr
+++ Lmdb/0.9.29/0.9.29.cr
@@ -8,7 +8,7 @@ class Target < ISM::Software
 
     def prepare
         @buildDirectory = true
-        @buildDirectoryName = "libraries/liblmdb"
+        @buildDirectoryName = "libraries/liblmdb/"
         super
     end
 
