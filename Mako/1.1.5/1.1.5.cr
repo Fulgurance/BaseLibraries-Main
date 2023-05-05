79199bcef5e5cc9e41075ea0bd29620451d90d58
Fulgurance <zohran.londais@gmail.com>
2023-05-05 12:50:53 +0100

    Patched not updated environment after installing python module manually

--- Mako/1.1.5/1.1.5.cr
+++ Mako/1.1.5/1.1.5.cr
@@ -4,6 +4,17 @@ class Target < ISM::Software
         super
 
         runPythonCommand(["setup.py","install","--optimize=1","--prefix=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr"],buildDirectoryPath)
+
+        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/python.sh")
+            copyFile("#{Ism.settings.rootPath}etc/profile.d/python.sh","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile.d/python.sh")
+        else
+            generateEmptyFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile.d/python.sh")
+        end
+
+        pythonData = <<-CODE
+        pathappend /usr/lib/python3.9/site-packages/Mako-1.1.5-py3.9egg PYTHONPATH
+        CODE
+        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh",pythonData)
     end
 
 end
