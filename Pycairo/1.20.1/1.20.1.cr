392e3bf85225aed697dd6031424f3d36aa680602
Fulgurance <zohran.londais@gmail.com>
2023-05-07 11:33:55 +0100

    Patched build phase

--- Pycairo/1.20.1/1.20.1.cr
+++ Pycairo/1.20.1/1.20.1.cr
@@ -1,9 +1,13 @@
 class Target < ISM::Software
+
+    def build
+        super
+
+        runPythonCommand(["setup.py","build"],buildDirectoryPath)
+    end
     
     def prepareInstallation
         super
-
-        runPythonCommand(["setup.py","build","--prefix=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr"],buildDirectoryPath)
 
         runPythonCommand(["setup.py","install","--optimize=1","--prefix=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr"],buildDirectoryPath)
 
