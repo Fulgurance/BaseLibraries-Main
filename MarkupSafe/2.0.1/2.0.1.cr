04a3ffc5375f3e5f38147a3d4592fc45c4f22796
fulgurance <zohran.londais@gmail.com>
2023-05-27 16:57:47 +0100

    Updated markupsafe installation implementation

--- MarkupSafe/2.0.1/2.0.1.cr
+++ MarkupSafe/2.0.1/2.0.1.cr
@@ -1,15 +1,26 @@
 class Target < ISM::Software
 
-    def build
-        super
-
-        runPythonCommand(["setup.py","build"],buildDirectoryPath)
-    end
-    
     def prepareInstallation
         super
 
-        runPythonCommand(["setup.py","install","--optimize=1","--prefix=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr"],buildDirectoryPath)
+        runPythonCommand(["setup.py","install","bdist"],buildDirectoryPath)
+
+        extractArchive("#{buildDirectoryPath}/dist/MarkupSafe-2.0.1.linux-x86_64.tar.gz")
+
+        copyDirectory("#{workDirectoryPath(false)}/usr","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr")
+
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile.d")
+
+        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/python.sh")
+            copyFile("#{Ism.settings.rootPath}etc/profile.d/python.sh","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile.d/python.sh")
+        else
+            generateEmptyFile("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile.d/python.sh")
+        end
+
+        pythonData = <<-CODE
+        pathappend /usr/lib/python3.9/site-packages/MarkupSafe-2.0.1-py3.9egg PYTHONPATH
+        CODE
+        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh",pythonData)
     end
 
 end
