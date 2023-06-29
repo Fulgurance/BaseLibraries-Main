ea46354bd3e78ae229ccc96af6b5aee7287957c8
Fulgurance <zohran.londais@gmail.com>
2023-06-29 08:51:10 +0100

    Patched paths

--- Mako/1.1.5/1.1.5.cr
+++ Mako/1.1.5/1.1.5.cr
@@ -20,7 +20,7 @@ class Target < ISM::Software
         pythonData = <<-CODE
         pathappend /usr/lib/python3.9/site-packages/Mako-1.1.5-py3.9.egg PYTHONPATH
         CODE
-        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh",pythonData)
+        fileUpdateContent("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/profile.d/python.sh",pythonData)
     end
 
 end
