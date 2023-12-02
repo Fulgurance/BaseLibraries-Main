2d9c20bfac46170f3f69ca6c3d00c9821fe3b890
Fulgurance <zohran.londais@gmail.com>
2023-12-02 23:27:50 +0000

    Added 32 and x32 bits support for Libffi

--- Libffi/3.4.4/3.4.4.cr
+++ Libffi/3.4.4/3.4.4.cr
@@ -1,5 +1,12 @@
 class Target < ISM::Software
 
+    def prepare
+        @buildDirectory = true
+        @buildDirectoryNames["32Bits"] = "mainBuild-32"
+        @buildDirectoryNames["x32Bits"] = "mainBuild-x32"
+        super
+    end
+
     def configure
         super
 
@@ -7,19 +14,67 @@ class Target < ISM::Software
                             "--disable-static",
                             "--with-gcc-arch=native",
                             "--disable-exec-static-tramp"],
-                            buildDirectoryPath)
+                            path: buildDirectoryPath(entry: "mainBuild"))
+
+        if option("32Bits")
+            configureSource([   "--host=i686-#{Ism.settings.targetName}-linux-gnux32",
+                                "--prefix=/usr",
+                                "--libdir=/usr/lib32",
+                                "--disable-static",
+                                "--with-gcc-arch=i686"],
+                                path: buildDirectoryPath(entry: "32Bits"),
+                                environment: {  "CC" = >"gcc -m32",
+                                                "CXX" => "g++ -m32"})
+        end
+
+        if option("x32Bits")
+            configureSource([   "--host=#{Ism.settings.target}x32",
+                                "--prefix=/usr",
+                                "--libdir=/usr/libx32",
+                                "--disable-static",
+                                "--with-gcc-arch=x86_64"],
+                                path: buildDirectoryPath(entry: "x32Bits"),
+                                environment: {  "CC" = >"gcc -mx32",
+                                                "CXX" => "g++ -mx32"})
+        end
     end
 
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        makeSource(path: buildDirectoryPath(entry: "mainBuild"))
+
+        if option("32Bits")
+            makeSource(path: buildDirectoryPath(entry: "32Bits"))
+        end
+
+        if option("x32Bits")
+            makeSource(path: buildDirectoryPath(entry: "x32Bits"))
+        end
     end
 
     def prepareInstallation
         super
 
-        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath(entry: "mainBuild"))
+
+        if option("32Bits")
+            makeDirectory("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr")
+
+            makeSource(["DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits","install"],buildDirectoryPath(entry: "32Bits"))
+
+            copyDirectory("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib32")
+        end
+
+        if option("x32Bits")
+            makeDirectory("#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr")
+
+            makeSource(["DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits","install"],buildDirectoryPath(entry: "x32Bits"))
+
+            copyDirectory("#{buildDirectoryPath(false, entry: "x32Bits")}/32Bits/usr/libx32","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/libx32")
+        end
     end
 
 end
