cde3457d6d34b75f920147611e94e24d45334206
Fulgurance <zohran.londais@gmail.com>
2024-01-06 19:25:41 +0000

    Added 32 bits support for ncurses

--- Ncurses/6.4.0/6.4.0.cr
+++ Ncurses/6.4.0/6.4.0.cr
@@ -1,6 +1,18 @@
 class Target < ISM::Software
     
     def prepare
+        if option("32Bits") || option("x32Bits")
+            @buildDirectory = true
+        end
+
+        if option("32Bits")
+            @buildDirectoryNames["32Bits"] = "mainBuild-32"
+        end
+
+        if option("x32Bits")
+            @buildDirectoryNames["x32Bits"] = "mainBuild-x32"
+        end
+
         if option("Pass1")
             @buildDirectory = true
             super
@@ -10,6 +22,75 @@ class Target < ISM::Software
             makeSource(["-C","progs","tic"],buildDirectoryPath)
         else
             super
+        end
+    end
+
+    def configure32Bits
+        if option("Pass1")
+            configureSource([   "--host=i686-#{Ism.settings.targetName}-linux-gnu",
+                                "--prefix=/usr",
+                                "--build=$(./config.guess)"
+                                "--libdir=/usr/lib32",
+                                "--mandir=/usr/share/man",
+                                "--with-shared",
+                                "--without-normal",
+                                "--with-cxx-shared",
+                                "--without-debug",
+                                "--without-ada",
+                                "--disable-stripping",
+                                "--enable-widec"],
+                                path: buildDirectoryPath(entry: "32Bits"),
+                                environment: {  "CC" =>"gcc -m32",
+                                                "CXX" => "g++ -m32"})
+        else
+            configureSource([   "--host=i686-#{Ism.settings.targetName}-linux-gnu",
+                                "--prefix=/usr",
+                                "--libdir=/usr/lib32",
+                                "--mandir=/usr/share/man",
+                                "--with-shared",
+                                "--without-normal",
+                                "--with-cxx-shared",
+                                "--without-debug",
+                                "--enable-pc-files"
+                                "--enable-widec",
+                                "--with-pkg-config-libdir=/usr/lib32/pkgconfig"],
+                                path: buildDirectoryPath(entry: "32Bits"),
+                                environment: {  "CC" =>"gcc -m32",
+                                                "CXX" => "g++ -m32"})
+        end
+    end
+
+    def configurex32Bits
+        if option("Pass1")
+            configureSource([   "--host=#{Ism.settings.target}X32",
+                                "--prefix=/usr",
+                                "--build=$(./config.guess)"
+                                "--libdir=/usr/lib32",
+                                "--mandir=/usr/share/man",
+                                "--with-shared",
+                                "--without-normal",
+                                "--with-cxx-shared",
+                                "--without-debug",
+                                "--without-ada",
+                                "--disable-stripping",
+                                "--enable-widec"],
+                                path: buildDirectoryPath(entry: "x32Bits"),
+                                environment: {  "CC" =>"gcc -mx32",
+                                                "CXX" => "g++ -mx32"})
+        else
+            configureSource([   "--host=#{Ism.settings.target}x32",
+                                "--prefix=/usr",
+                                "--libdir=/usr/lib32",
+                                "--mandir=/usr/share/man",
+                                "--with-shared",
+                                "--without-normal",
+                                "--without-debug",
+                                "--enable-pc-files"
+                                "--enable-widec",
+                                "--with-pkg-config-libdir=/usr/libx32/pkgconfig"],
+                                path: buildDirectoryPath(entry: "x32Bits"),
+                                environment: {  "CC" =>"gcc -mx32",
+                                                "CXX" => "g++ -mx32"})
         end
     end
 
@@ -42,12 +123,28 @@ class Target < ISM::Software
                                 "--with-pkg-config-libdir=/usr/lib/pkgconfig"],
                                 buildDirectoryPath)
         end
+
+        if option("32Bits")
+            configure32Bits
+        end
+
+        if option("x32Bits")
+            configurex32Bits
+        end
     end
     
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        makeSource(path: buildDirectoryPath(entry: "mainBuild"))
+
+        if option("32Bits")
+            makeSource(path: buildDirectoryPath(entry: "32Bits"))
+        end
+
+        if option("x32Bits")
+            makeSource(path: buildDirectoryPath(entry: "x32Bits"))
+        end
     end
     
     def prepareInstallation
@@ -64,6 +161,72 @@ class Target < ISM::Software
             fileAppendData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/lib/libmenu.so","INPUT(-lmenuw)")
             fileAppendData("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/lib/libcursesw.so","INPUT(-lncursesw)")
         end
+
+        if option("32Bits")
+            prepareInstallation32Bits
+        end
+
+        if option("x32Bits")
+            prepareInstallationx32Bits
+        end
+    end
+
+    def prepareInstallation32Bits
+        if option("Pass1")
+
+            makeSource( ["DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits",
+                        "TIC_PATH=$(pwd)/build/progs/tic",
+                        "install"],
+                        path: buildDirectoryPath(entry: "32Bits"))
+        else
+
+            makeSource( ["DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits",
+                        "install"],
+                        path: buildDirectoryPath(entry: "32Bits"))
+
+            makeDirectory("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32/pkgconfig")
+
+            fileAppendData("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32/libncurses.so","INPUT(-lncursesw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32/libform.so","INPUT(-lformw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32/libpanel.so","INPUT(-lpanelw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32/libmenu.so","INPUT(-lmenuw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32/libcursesw.so","INPUT(-lncursesw)")
+        end
+
+        makeDirectory("#{buildDirectoryPath(false, entry: "32Bits")}/32Bits")
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr")
+
+        copyDirectory(  "#{buildDirectoryPath(false, entry: "32Bits")}/32Bits/usr/lib32",
+                        "#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/lib32")
+    end
+
+    def prepareInstallationx32Bits
+        if option("Pass1")
+
+            makeSource( ["DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits",
+                        "TIC_PATH=$(pwd)/build/progs/tic",
+                        "install"],
+                        path: buildDirectoryPath(entry: "x32Bits"))
+        else
+
+            makeSource( ["DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits",
+                        "install"],
+                        path: buildDirectoryPath(entry: "x32Bits"))
+
+            makeDirectory("#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits/usr/libx32/pkgconfig")
+
+            fileAppendData("#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits/usr/libx32/libncurses.so","INPUT(-lncursesw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "x32Bits")}/32Bits/usr/libx32/libform.so","INPUT(-lformw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "x32Bits")}/32Bits/usr/libx32/libpanel.so","INPUT(-lpanelw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "x32Bits")}/32Bits/usr/libx32/libmenu.so","INPUT(-lmenuw)")
+            fileAppendData("#{buildDirectoryPath(false, entry: "x32Bits")}/32Bits/usr/libx32/libcursesw.so","INPUT(-lncursesw)")
+        end
+
+        makeDirectory("#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits")
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr")
+
+        copyDirectory(  "#{buildDirectoryPath(false, entry: "x32Bits")}/x32Bits/usr/libx32",
+                        "#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}/usr/libx32")
     end
 
     def install
@@ -77,6 +240,29 @@ class Target < ISM::Software
             makeLink("libncurses.so","#{Ism.settings.rootPath}usr/lib/libcurses.so",:symbolicLinkByOverwrite)
         end
 
+        if option("32Bits")
+            if option("Pass1")
+                makeLink("libncursesw.so","#{Ism.settings.rootPath}usr/lib32/libcursesw.so",:symbolicLinkByOverwrite)
+            else
+                makeLink("ncursesw.pc","#{Ism.settings.rootPath}usr/lib32/pkgconfig/ncurses.pc",:symbolicLinkByOverwrite)
+                makeLink("formw.pc","#{Ism.settings.rootPath}usr/lib32/pkgconfig/form.pc",:symbolicLinkByOverwrite)
+                makeLink("panelw.pc","#{Ism.settings.rootPath}usr/lib32/pkgconfig/panel.pc",:symbolicLinkByOverwrite)
+                makeLink("menuw.pc","#{Ism.settings.rootPath}usr/lib32/pkgconfig/menu.pc",:symbolicLinkByOverwrite)
+                makeLink("libncurses.so","#{Ism.settings.rootPath}usr/lib32/libcurses.so",:symbolicLinkByOverwrite)
+            end
+        end
+
+        if option("x32Bits")
+            if option("Pass1")
+                makeLink("libncursesw.so","#{Ism.settings.rootPath}usr/libx32/libcursesw.so",:symbolicLinkByOverwrite)
+            else
+                makeLink("ncursesw.pc","#{Ism.settings.rootPath}usr/libx32/pkgconfig/ncurses.pc",:symbolicLinkByOverwrite)
+                makeLink("formw.pc","#{Ism.settings.rootPath}usr/libx32/pkgconfig/form.pc",:symbolicLinkByOverwrite)
+                makeLink("panelw.pc","#{Ism.settings.rootPath}usr/libx32/pkgconfig/panel.pc",:symbolicLinkByOverwrite)
+                makeLink("menuw.pc","#{Ism.settings.rootPath}usr/libx32/pkgconfig/menu.pc",:symbolicLinkByOverwrite)
+                makeLink("libncurses.so","#{Ism.settings.rootPath}usr/libx32/libcurses.so",:symbolicLinkByOverwrite)
+            end
+        end
     end
 
 end
