3874ed42b6d76dad1486af75e6aa48d5b6e14eae
Fulgurance <zohran.londais@gmail.com>
2024-01-08 17:11:19 +0000

    Added new software lz4

---
+++ Lz4/1.9.4/1.9.4.cr
@@ -0,0 +1,15 @@
+class Target < ISM::Software
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
