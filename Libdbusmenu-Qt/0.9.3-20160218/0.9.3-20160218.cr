cf8060af727f5add9f2b4a4ef1b59a2a71d0f01d
Fulgurance <zohran.londais@gmail.com>
2024-06-01 22:16:24 +0100

    Added new library

---
+++ Libdbusmenu-Qt/0.9.3-20160218/0.9.3-20160218.cr
@@ -0,0 +1,31 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand([   "-DCMAKE_INSTALL_PREFIX=/usr",
+                            "-DCMAKE_BUILD_TYPE=Release",
+                            "-DWITH_DOC=OFF",
+                            "-Wno-dev",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
