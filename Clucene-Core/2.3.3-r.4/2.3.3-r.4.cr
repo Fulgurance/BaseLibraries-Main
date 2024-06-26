67e59632cde7a71d1cf0d681c8dcf54a895ce0da
Fulgurance <zohran.londais@gmail.com>
2024-06-26 14:26:29 +0100

    Added clucene-core

---
+++ Clucene-Core/2.3.3-r.4/2.3.3-r.4.cr
@@ -0,0 +1,30 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr    \
+                                    -DBUILD_CONTRIBS_LIB=ON         \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
