1f31739a6c2fffff3eda8db896274f7cdf2dbd8e
Fulgurance <zohran.londais@gmail.com>
2024-09-09 19:58:49 +0100

    Added PyQt6 and PyQt6-WebEngine

---
+++ PyQt6/6.7.1/6.7.1.cr
@@ -0,0 +1,32 @@
+class Target < ISM::Software
+    
+    def prepareInstallation
+        super
+
+        runPythonCommand(   arguments:  "setup.py install bdist",
+                            path:       buildDirectoryPath)
+
+        runTarCommand(  arguments:  "-xf pyqt6-6.7.1.linux-x86_64.tar.gz",
+                        path:       "#{buildDirectoryPath}/dist/")
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr")
+
+        copyDirectory(  "#{buildDirectoryPath}/dist/",
+                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr")
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d")
+
+        if File.exists?("#{Ism.settings.rootPath}etc/profile.d/python.sh")
+            copyFile(   "/etc/profile.d/python.sh",
+                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh")
+        else
+            generateEmptyFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh")
+        end
+
+        pythonData = <<-CODE
+        pathappend /usr/lib/python3.11/site-packages/pyqt6-6.7.1-py3.11-linux-x86_64.egg PYTHONPATH
+        CODE
+        fileUpdateContent("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh",pythonData)
+    end
+
+end
