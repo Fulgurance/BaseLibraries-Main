85ba6e8d0b8afb16cef88ea71d30a7f80917ba9d
Fulgurance <zohran.londais@gmail.com>
2024-10-21 13:50:27 +0100

    Added proper support for python modules

--- PyQt6/6.7.1/6.7.1.cr
+++ PyQt6/6.7.1/6.7.1.cr
@@ -1,14 +1,2 @@
-class Target < ISM::Software
-    
-    def prepareInstallation
-        super
-
-        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/python3.11/sites-packages")
-
-        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d")
-
-        copyDirectory(  "#{buildDirectoryPath}/*",
-                        "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/python3.11/sites-packages/")
-    end
-
+class PythonPackageSoftware < ISM::SemiVirtualSoftware
 end
