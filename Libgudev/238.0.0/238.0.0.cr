c2d0abd6b3e7e540af6fbc2fd08cde39f2e3e9c0
Fulgurance <zohran.londais@gmail.com>
2024-11-08 23:25:36 +0000

    Added gobject support

--- Libgudev/238.0.0/238.0.0.cr
+++ Libgudev/238.0.0/238.0.0.cr
@@ -11,6 +11,7 @@ class Target < ISM::Software
         runMesonCommand(arguments:  "setup --reconfigure    \
                                     --prefix=/usr           \
                                     --buildtype=release     \
+                                    -Dintrospection=#{option("Gobject-Introspection") ? "enabled" : "disabled"} \
                                     ..",
                         path:       buildDirectoryPath)
     end
