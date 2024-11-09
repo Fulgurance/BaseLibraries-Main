63bb814c814a178ed5bd88325637ca5ac21466b2
Fulgurance <zohran.londais@gmail.com>
2024-11-09 23:00:14 +0000

    Added experimental patch

--- Qca/2.3.9/2.3.9.cr
+++ Qca/2.3.9/2.3.9.cr
@@ -8,12 +8,7 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=\"/usr\"    \
-                                    -DCMAKE_BUILD_TYPE=Release      \
-                                    -DQT6=ON                        \
-                                    -DQCA_INSTALL_IN_QT_PREFIX=ON   \
-                                    -DQCA_MAN_INSTALL_DIR=\"/usr/share/man\"   \
-                                    ..",
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DQT6=ON -DQCA_INSTALL_IN_QT_PREFIX=ON -DQCA_MAN_INSTALL_DIR=/usr/share/man ..",
                         path:       buildDirectoryPath)
     end
     
