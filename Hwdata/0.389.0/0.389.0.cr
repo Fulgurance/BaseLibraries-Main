e39e038d38a3ffe9e4539451f60b7acf50f961d4
Fulgurance <zohran.londais@gmail.com>
2024-11-11 21:38:55 +0000

    Added hwdata software

---
+++ Hwdata/0.389.0/0.389.0.cr
@@ -0,0 +1,18 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        configureSource(arguments:  "--prefix=/usr  \
+                                    --disable-blacklist",
+                        path:       buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
