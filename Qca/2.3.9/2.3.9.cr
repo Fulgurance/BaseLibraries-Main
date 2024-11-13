aedb47a8fa5fae9f0ecd107f8a67b6aaf9f4d479
Fulgurance <zohran.londais@gmail.com>
2024-11-13 08:33:00 +0000

    Updated installer

--- Qca/2.3.9/2.3.9.cr
+++ Qca/2.3.9/2.3.9.cr
@@ -8,11 +8,11 @@ class Target < ISM::Software
     def configure
         super
 
-        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr                \
-                                    -D CMAKE_BUILD_TYPE=Release                 \
-                                    -D QT6=ON                                   \
-                                    -D QCA_INSTALL_IN_QT_PREFIX=ON              \
-                                    -D QCA_MAN_INSTALL_DIR:PATH=/usr/share/man  \
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr               \
+                                    -DCMAKE_BUILD_TYPE=Release                 \
+                                    -DQT6=ON                                   \
+                                    -DQCA_INSTALL_IN_QT_PREFIX=ON              \
+                                    -DQCA_MAN_INSTALL_DIR:PATH=/usr/share/man  \
                                     ..",
                         path:       buildDirectoryPath)
     end
