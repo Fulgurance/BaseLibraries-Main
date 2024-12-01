22c6ea08a22901a850ebc03e0c23980b46e7f3a4
Fulgurance <zohran.londais@gmail.com>
2024-12-01 19:53:30 +0000

    Added 2 new libraries

---
+++ Libaccounts-Qt/1.17.0/1.17.0.cr
@@ -0,0 +1,28 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runQmakeCommand(arguments:  "..",
+                        path:       buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "INSTALL_ROOT=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
