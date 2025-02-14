de5f430b1626ba2714ce47869a7818f0a750f81c
Fulgurance <zohran.londais@gmail.com>
2025-02-14 10:45:58 +0000

    Added 2 new python libraries

---
+++ Zenlib/3.1.5/3.1.5.cr
@@ -0,0 +1,40 @@
+class Target < ISM::Software
+    
+    def prepareInstallation
+        super
+
+        majorPythonVersion = softwareMajorVersion("@ProgrammingLanguages-Main:Python")
+        minorPythonVersion = softwareMinorVersion("@ProgrammingLanguages-Main:Python")
+        pythonVersion = "#{majorPythonVersion}.#{minorPythonVersion}"
+
+        packagesPath = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/lib/python#{pythonVersion}/site-packages"
+
+        makeDirectory(packagesPath)
+
+        runPipCommand(  arguments:  "install --root-user-action=ignore --no-dependencies --target \"#{packagesPath}\" .",
+                        version:    pythonVersion)
+
+        directoryContent(packagesPath, matchHidden: true).each do |filePath|
+
+            if filePath.squeeze("/") == "#{packagesPath}/share".squeeze("/")
+                destinationPath = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr"
+
+                makeDirectory(destinationPath)
+
+                moveFile(   path:       filePath,
+                            newPath:    destinationPath)
+            end
+
+            if filePath.squeeze("/") == "#{packagesPath}/bin".squeeze("/")
+                destinationPath = "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr"
+
+                makeDirectory(destinationPath)
+
+                moveFile(   path:       filePath,
+                            newPath:    destinationPath)
+            end
+
+        end
+    end
+
+end
