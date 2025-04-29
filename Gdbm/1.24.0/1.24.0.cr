82b6e22ed570d9cb5d0ac02878b632d833ef8377
Fulgurance <zohran.londais@gmail.com>
2025-04-29 13:59:23 +0100

    Set target triple

--- Gdbm/1.24.0/1.24.0.cr
+++ Gdbm/1.24.0/1.24.0.cr
@@ -2,8 +2,11 @@ class Target < ISM::Software
 
     def configure
         super
-        configureSource(arguments:  "--prefix=/usr      \
-                                    --disable-static    \
+        configureSource(arguments:  "--prefix=/usr                          \
+                                    --host=#{Ism.settings.systemTarget}     \
+                                    --build=#{Ism.settings.systemTarget}    \
+                                    --target=#{Ism.settings.systemTarget}   \
+                                    --disable-static                        \
                                     --enable-libgdbm-compat",
                         path:       buildDirectoryPath)
     end
