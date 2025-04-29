adae189550260848affd2889ece6f93bcfb479fa
Fulgurance <zohran.londais@gmail.com>
2025-04-29 13:56:46 +0100

    Patched target triple

--- Libffi/3.4.5/3.4.5.cr
+++ Libffi/3.4.5/3.4.5.cr
@@ -3,9 +3,12 @@ class Target < ISM::Software
     def configure
         super
 
-        configureSource(arguments:  "--prefix=/usr          \
-                                    --disable-static        \
-                                    --with-gcc-arch=native  \
+        configureSource(arguments:  "--prefix=/usr                          \
+                                    --host=#{Ism.settings.systemTarget}     \
+                                    --build=#{Ism.settings.systemTarget}    \
+                                    --target=#{Ism.settings.systemTarget}   \
+                                    --disable-static                        \
+                                    --with-gcc-arch=native                  \
                                     --disable-exec-static-tramp",
                         path:       buildDirectoryPath)
     end
