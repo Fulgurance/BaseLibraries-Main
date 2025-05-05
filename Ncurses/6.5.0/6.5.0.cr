dacf6530a06383e6233919bd0ef67f1c26da122e
Fulgurance <zohran.londais@gmail.com>
2025-05-05 13:13:01 +0100

    Patched build

--- Ncurses/6.5.0/6.5.0.cr
+++ Ncurses/6.5.0/6.5.0.cr
@@ -23,7 +23,7 @@ class Target < ISM::Software
         if option("Pass1")
             configureSource(arguments:  "--prefix=/usr                                                  \
                                         --host=#{Ism.settings.chrootTarget}                             \
-                                        --build=#{Ism.settings.systemTarget(relatedToChroot: false)}    \
+                                        --build=$(./config.guess)                                       \
                                         --mandir=/usr/share/man                                         \
                                         --with-manpage-format=normal                                    \
                                         --with-shared                                                   \
