5b82cd1545de0852f996ee616532039699dbbd42
Fulgurance <zohran.londais@gmail.com>
2025-06-05 19:54:04 +0100

    Removed non existent dir

--- Openldap/2.6.8/2.6.8.cr
+++ Openldap/2.6.8/2.6.8.cr
@@ -84,12 +84,10 @@ class Target < ISM::Software
     def deploy
         super
 
-        runChownCommand("ldap:ldap /var/lib/openldap")
         runChownCommand("ldap:ldap /etc/openldap/slapd.d")
         runChownCommand("root:ldap /etc/openldap/slapd.conf")
         runChownCommand("root:ldap /etc/openldap/slapd.ldif")
 
-        runChmodCommand("0700 /var/lib/openldap")
         runChmodCommand("0700 /etc/openldap/slapd.d")
         runChmodCommand("0640 /etc/openldap/slapd.conf")
         runChmodCommand("0640 /etc/openldap/slapd.ldif")
