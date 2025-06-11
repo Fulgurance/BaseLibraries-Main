e6f8f376351305affe03e960d6c33514a0ea5ca0
Fulgurance <zohran.londais@gmail.com>
2025-06-11 15:47:16 +0100

    Patched installation process

--- Pycpio/1.4.2/1.4.2.cr
+++ Pycpio/1.4.2/1.4.2.cr
@@ -12,6 +12,7 @@ class Target < ISM::Software
         makeDirectory(packagesPath)
 
         runPipCommand(  arguments:  "install --root-user-action=ignore --no-dependencies --target \"#{packagesPath}\" #{mainWorkDirectoryPath}",
+                        path: "/var/lib/ism",
                         version:    pythonVersion)
 
         directoryContent(packagesPath, matchHidden: true).each do |filePath|
