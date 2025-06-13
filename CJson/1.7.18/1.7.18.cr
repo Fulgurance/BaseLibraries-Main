ddd447fb73641e2088294cff2038c07d8cc1d08e
Fulgurance <zohran.londais@gmail.com>
2025-06-13 18:49:53 +0100

    Added new library

---
+++ CJson/1.7.18/1.7.18.cr
@@ -0,0 +1,27 @@
+class Target < ISM::Software
+
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr    \
+                                    -DCMAKE_BUILD_TYPE=Release      \
+                                    -DENABLE_CJSON_UTILS=On         \
+                                    -DCMAKE_INSTALL_LIBDIR=lib      \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
