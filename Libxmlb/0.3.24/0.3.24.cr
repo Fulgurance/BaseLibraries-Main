b84ee4cce80a5166712def378a77fde90865e988
Fulgurance <zohran.londais@gmail.com>
2025-11-03 09:52:19 +0000

    Added libxmlb

---
+++ Libxmlb/0.3.24/0.3.24.cr
@@ -0,0 +1,32 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+
+    def configure
+        super
+
+        runMesonCommand(arguments:  "setup --reconfigure    \
+                                    --prefix=/usr           \
+                                    --buildtype=release     \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(arguments:      "install",
+                        path:           buildDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
