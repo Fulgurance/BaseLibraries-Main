ef21d0518278e5810a9a47f6a05c6be46c57dc8d
Fulgurance <zohran.londais@gmail.com>
2025-12-09 20:31:03 +0000

    Removed buggy argument

--- Libclc/21.1.5/Installer.cr
+++ Libclc/21.1.5/Installer.cr
@@ -10,7 +10,6 @@ class Target < ISM::Software
 
         runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr                                                        \
                                     -DCMAKE_BUILD_TYPE=Release                                                          \
-                                    -DLLVM_SPIRV=/usr                                                                   \
                                     -DLIBCLC_TARGETS_TO_BUILD=\"nvptx--;nvptx64--;nvptx--nvidiacl;nvptx64--nvidiacl;spirv-mesa3d-;spirv64-mesa3d-\"   \
                                     -G Ninja ..",
                         path:       buildDirectoryPath,
